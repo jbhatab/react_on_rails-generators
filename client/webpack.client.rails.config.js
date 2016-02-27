94e16805ca7747213694d7f4e49af2212adbd0aa
jbhatab <jbhatab@gmail.com>
2016-02-26 19:47:31 -0600

    Default server rendered component installer

---
+++ client/webpack.client.rails.config.js
@@ -0,0 +1,89 @@
+// Run like this:
+// cd client && npm run build:dev:client
+// Note that Foreman (Procfile.dev) has also been configured to take care of this.
+
+// NOTE: All style sheets handled by the asset pipeline in rails
+const webpack = require('webpack');
+const path = require('path');
+
+const devBuild = process.env.NODE_ENV !== 'production';
+const nodeEnv = devBuild ? 'development' : 'production';
+
+module.exports = {
+
+  // the project dir
+  context: __dirname,
+  entry: {
+
+    // See use of 'vendor' in the CommonsChunkPlugin inclusion below.
+    vendor: [
+      'es5-shim/es5-shim',
+      'es5-shim/es5-sham',
+      'babel-polyfill',
+      'jquery',
+      'jquery-ujs',
+    ],
+
+    // This will contain the app entry points defined by webpack.hot.config and
+    // webpack.rails.config
+    app: [
+      './app/bundles/HelloWorld/startup/clientRegistration',
+    ],
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    alias: {
+      lib: path.join(process.cwd(), 'app', 'lib'),
+      react: path.resolve('./node_modules/react'),
+      'react-dom': path.resolve('./node_modules/react-dom'),
+    },
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(nodeEnv),
+      },
+    }),
+
+    // https://webpack.github.io/docs/list-of-plugins.html#2-explicit-vendor-chunk
+    new webpack.optimize.CommonsChunkPlugin({
+
+      // This name 'vendor' ties into the entry definition
+      name: 'vendor',
+
+      // We don't want the default vendor.js name
+      filename: 'vendor-bundle.js',
+
+      // Passing Infinity just creates the commons chunk, but moves no modules into it.
+      // In other words, we only put what's in the vendor entry definition in vendor-bundle.js
+      minChunks: Infinity,
+    }),
+  ],
+  module: {
+    loaders: [
+
+      // Not all apps require jQuery. Many Rails apps do, such as those using TurboLinks or
+      // bootstrap js
+      { test: require.resolve('jquery'), loader: 'expose?jQuery' },
+      { test: require.resolve('jquery'), loader: 'expose?$' },
+      { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: require.resolve('react'), loader: 'imports?shim=es5-shim/es5-shim&sham=es5-shim/es5-sham' },
+      { test: require.resolve('jquery-ujs'), loader: 'imports?jQuery=jquery' },
+    ],
+  },
+  output: {
+    filename: '[name]-bundle.js',
+    path: '../app/assets/javascripts/generated',
+  },
+};
+
+
+if (devBuild) {
+  console.log('Webpack dev build for Rails'); // eslint-disable-line no-console
+  module.exports.devtool = 'eval-source-map';
+} else {
+  config.plugins.push(
+    new webpack.optimize.DedupePlugin()
+  );
+  console.log('Webpack production build for Rails'); // eslint-disable-line no-console
+}
