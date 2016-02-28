3bbd77bff6703c2e237876f887a776b6077390f6
jbhatab <jbhatab@gmail.com>
2016-02-27 21:07:58 -0600

    combined all startup files into one

--- client/app/bundles/HelloWorld/startup/HelloWorldApp.jsx
+++ client/app/bundles/HelloWorld/startup/HelloWorldApp.jsx
@@ -1,5 +1,11 @@
 import ReactOnRails from 'react-on-rails';
-import HelloWorldApp from './HelloWorldAppClient';
+import React from 'react';
+import HelloWorld from '../containers/HelloWorld';
+
+export default (props) => (
+  <HelloWorld {...props} />
+);
+
 
 // This is how react_on_rails can see the HelloWorldApp in the browser.
 ReactOnRails.register({ HelloWorldApp });
